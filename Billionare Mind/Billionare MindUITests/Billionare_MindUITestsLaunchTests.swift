e1787ace588d153de819be8162cf50df1427908f
BryantheNext <77672@psdschools.org>
2024-05-14 09:31:28 -0600

    Add files via upload

---
+++ Billionare Mind/Billionare MindUITests/Billionare_MindUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Billionare_MindUITestsLaunchTests.swift
+//  Billionare MindUITests
+//
+//  Created by Leos, Bryan - Student on 3/5/24.
+//
+
+import XCTest
+
+final class Billionare_MindUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
